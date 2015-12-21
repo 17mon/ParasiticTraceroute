478f107ca92f99fbf37ada1ca937ddd2b2573dca
ilsanbao <ilsanboo@gmail.com>
2015-12-21 13:10:24 +0800

    add

--- trace/trace.go
+++ trace/trace.go
@@ -28,9 +28,9 @@
 package trace
 
 import (
-	"code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/layers"
-	"code.google.com/p/gopacket/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 	"fmt"
 	"github.com/openshift/geard/pkg/go-netfilter-queue"
 	"log"
